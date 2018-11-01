5a662aa85fc03e92d03358688750d3dcd8a2b623
Justin Shepard <justin.shepard@gmail.com>
2018-10-31 19:56:06 -0700

    Extend FlatSpec
    Updated the CuckooHashMapSpec to exted FlatSpec.

--- cuckoo-hash/src/test/scala/net/justinshepard/cuckoohash/CuckooHashMapSpec.scala
+++ cuckoo-hash/src/test/scala/net/justinshepard/cuckoohash/CuckooHashMapSpec.scala
@@ -1,8 +1,10 @@
 package net.justinshepard.cuckoohash
 
+import org.scalatest.FlatSpec
+
 /**
   * Created by justinshepard on 2/7/17.
   */
-class CuckooHashMapSpec {
+class CuckooHashMapSpec extends FlatSpec {
 
 }
